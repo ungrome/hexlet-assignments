71a387e508d7fa164d759e5a93235452a1b33065
@hexlet/cli <support@hexlet.io>
2024-09-05 15:22:59 +0300

    submit java-web-ru/get-form

---
+++ java-web-ru/get-form/src/main/java/exercise/dto/users/UsersPage.java
@@ -0,0 +1,15 @@
+package exercise.dto.users;
+
+import exercise.model.User;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+@Getter
+@AllArgsConstructor
+// BEGIN
+public class UsersPage {
+    public List<User> users;
+    public String term;
+}
+// END
