0e795f2d41704b0ef769a99be0292d68ee70adfa
@hexlet/cli <support@hexlet.io>
2025-02-28 13:53:17 +0300

    submit java-spring-boot-ru/dto-to-entity

---
+++ java-spring-boot-ru/dto-to-entity/src/main/java/exercise/controller/ContactsController.java
@@ -0,0 +1,43 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
+import exercise.model.Contact;
+import exercise.repository.ContactRepository;
+import exercise.dto.ContactDTO;
+import exercise.dto.ContactCreateDTO;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactsController {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    // BEGIN
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContactDTO create(@RequestBody ContactCreateDTO data) {
+        var contact = new Contact();
+        contact.setFirstName(data.getFirstName());
+        contact.setLastName(data.getLastName());
+        contact.setPhone(data.getPhone());
+        contactRepository.save(contact);
+        var contactDTO = toDTO(contact);
+        return contactDTO;
+
+    }
+
+    public ContactDTO toDTO(Contact contact) {
+        var dto = new ContactDTO();
+        dto.setFirstName(contact.getFirstName());
+        dto.setLastName(contact.getLastName());
+        dto.setPhone(contact.getPhone());
+        dto.setUpdatedAt(contact.getUpdatedAt());
+        dto.setCreatedAt(contact.getCreatedAt());
+        return dto;
+    }
+    // END
+}
