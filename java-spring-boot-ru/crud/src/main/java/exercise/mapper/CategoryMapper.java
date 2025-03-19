1c6e56a8094f8fbc097680340295d005118fcb93
@hexlet/cli <support@hexlet.io>
2025-03-19 12:03:32 +0300

    submit java-spring-boot-ru/crud

---
+++ java-spring-boot-ru/crud/src/main/java/exercise/mapper/CategoryMapper.java
@@ -0,0 +1,26 @@
+package exercise.mapper;
+
+import exercise.dto.CategoryCreateDTO;
+import exercise.dto.CategoryDTO;
+import exercise.model.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+// BEGIN
+@Mapper(
+        uses = { JsonNullableMapper.class, ReferenceMapper.class },
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class CategoryMapper {
+    public abstract Category map(CategoryCreateDTO categoryCreateDTO);
+
+    public abstract CategoryDTO map(Category category);
+
+    public abstract Category map(CategoryDTO categoryDTO);
+
+}
+// END
---
+++ java-spring-boot-ru/crud/src/main/java/exercise/mapper/ProductMapper.java
@@ -0,0 +1,32 @@
+package exercise.mapper;
+
+import exercise.dto.ProductCreateDTO;
+import exercise.dto.ProductDTO;
+import exercise.dto.ProductUpdateDTO;
+import exercise.model.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+// BEGIN
+@Mapper(
+        uses = { JsonNullableMapper.class, ReferenceMapper.class },
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class ProductMapper {
+    @Mapping(target = "category", source = "categoryId")
+    public abstract Product map(ProductCreateDTO productCreateDto);
+
+    @Mapping(target = "categoryId", source = "category.id")
+    @Mapping(target = "categoryName", source = "category.name")
+    public abstract ProductDTO map(Product product);
+
+    @Mapping(target = "category", source = "categoryId")
+    public abstract void update(ProductUpdateDTO dto, @MappingTarget Product product);
+}
+// END
